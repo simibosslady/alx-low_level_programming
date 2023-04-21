b05a862a82875201a0f2929bbd958fc655283ed4
simibosslady <temmytope841@gmail.com>
2023-04-21 12:02:55 -0700

    reverse alpabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reverse order
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
