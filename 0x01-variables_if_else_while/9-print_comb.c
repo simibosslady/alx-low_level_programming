04db4caf6a737372aa904259272672b67f60b4ed
simibosslady <temmytope841@gmail.com>
2023-04-21 13:38:23 -0700

    print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 /**
- * main - cobination of letters and numbers
+ * main - enttry
  * Return: 0
  */
 int main(void)
 {
-	int n;
-	int m;
+	int n = 48;
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-	}:
-	for (m = 97; m <= 102; m++)
-	{
-		putchar(m);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
