9214b1472fe430d2f5ebc8b994159fbc91455b0d
simibosslady <temmytope841@gmail.com>
2023-04-21 18:05:32 -0700

    combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 /**
  * main - entry point
+ * description - print three numbers
  * Return: 0
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+int n = '0';
+int m = '0';
 
+while (n <= '7')
+{
+while (m <= '8')
+{
+if (n < m)
+{
+putchar(n);
+putchar(m);
+if (!(n == '7' && m == '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+m++;
+}
+m = '0';
+n++;
+}
+putchar('\n');
+return (0);
+}
