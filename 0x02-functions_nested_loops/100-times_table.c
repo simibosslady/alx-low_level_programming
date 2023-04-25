bf9b8835dcd8641fc6f4efedd5e85b8418b9fa49
simibosslady <temmytope841@gmail.com>
2023-04-25 16:52:23 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,7 +24,8 @@ void print_times_table(int n)
 				if (prod <= 0)
 					_putchar(' ');
 				if (prod >= 100)
-				{_putchar((prod / 100) + '0');
+				{
+					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
 					else if (prod <= 99 && prod >= 10)
