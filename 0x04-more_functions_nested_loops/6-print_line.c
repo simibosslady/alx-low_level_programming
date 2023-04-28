15313e2df03b58f6239a9394efed5655aed035c4
simibosslady <temmytope841@gmail.com>
2023-04-28 13:08:48 -0700

    prinnt line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - to prinnt straight linne
+ * @n : number of times
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
