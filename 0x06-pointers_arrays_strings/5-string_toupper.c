9e491ceac0145a6d0644c56ac58b24470a278a11
simibosslady <temmytope841@gmail.com>
2023-05-04 07:48:03 -0700

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase
+ * @str: the string to be changed
+ * Return: apointer to be the changed string
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
+index++;
+}
+return (str);
+}
